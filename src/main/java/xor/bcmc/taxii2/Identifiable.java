2cfb7f0e88a234fa7f6a49c7bf40cdc967ac3d8d
robotad <dadamos@bcmcgroup.com>
2017-12-18 12:15:39 -0500

    Add warning: do not create an object with id = "_abilities"

--- src/main/java/xor/bcmc/taxii2/Identifiable.java
+++ src/main/java/xor/bcmc/taxii2/Identifiable.java
@@ -2,5 +2,9 @@ package xor.bcmc.taxii2;
 
 public interface Identifiable<E> {
     E getId();
+
+    // Warning: the Authorization json uses "_abilities" as
+    // a reserved word, therefore, no object id of string
+    // "_abilities" should be created.
     void setId (E id);
 }
