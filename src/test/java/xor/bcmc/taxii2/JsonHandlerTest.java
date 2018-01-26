93da7e1e0037cdd952c19b68d1731a4f6a6310ef
robotad <dadamos@bcmcgroup.com>
2018-01-26 10:14:13 -0500

    Add test for Date serialization

--- src/test/java/xor/bcmc/taxii2/JsonHandlerTest.java
+++ src/test/java/xor/bcmc/taxii2/JsonHandlerTest.java
@@ -5,8 +5,8 @@ import com.google.gson.JsonParser;
 import com.google.gson.annotations.Expose;
 import org.junit.Test;
 
-import java.sql.Timestamp;
 import java.time.ZonedDateTime;
+import java.util.Date;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.IsEqual.equalTo;
@@ -14,11 +14,11 @@ import static org.hamcrest.core.IsEqual.equalTo;
 public class JsonHandlerTest {
 
     @Test
-    public void test1 () {
+    public void testZonedDateTime1 () {
         String timestampStr = "2016-01-01T01:01:01Z";
         ZonedDateTime timestamp = ZonedDateTime.parse(timestampStr);
 
-        TimestampJson timestampJson = new TimestampJson(timestamp);
+        ZonedDateTimeJson timestampJson = new ZonedDateTimeJson(timestamp);
         JsonHandler jsonHandler = JsonHandler.getInstance();
 
         JsonObject timestampJsonObject = new JsonParser().parse(jsonHandler.toJson(timestampJson)).getAsJsonObject();
@@ -27,24 +27,24 @@ public class JsonHandlerTest {
     }
 
     @Test
-    public void test2 () {
+    public void testZonedDateTime2 () {
         String timestampStr = "2016-01-01T01:01:01.000001Z";
         ZonedDateTime timestamp = ZonedDateTime.parse(timestampStr);
 
-        TimestampJson timestampJson = new TimestampJson(timestamp);
+        ZonedDateTimeJson timestampJson = new ZonedDateTimeJson(timestamp);
 
         JsonObject timestampJsonObject = new JsonParser().parse(JsonHandler.getInstance().toJson(timestampJson)).getAsJsonObject();
 
         assertThat(timestampJsonObject.get("time").getAsString(), equalTo("2016-01-01T01:01:01.000001Z"));
     }
 
-    class TimestampJson {
+    class ZonedDateTimeJson {
         @Expose
         private ZonedDateTime time;
 
-        public TimestampJson () {}
+        public ZonedDateTimeJson() {}
 
-        public TimestampJson(ZonedDateTime time) {
+        public ZonedDateTimeJson(ZonedDateTime time) {
             this.time = time;
         }
 
@@ -53,6 +53,39 @@ public class JsonHandlerTest {
         }
 
         public void setTime(ZonedDateTime time) {
+            this.time = time;
+        }
+    }
+
+    @Test
+    public void testDate1 () {
+        // Note: Date has milliseconds precision - it will truncate
+        String timestampStr = "2016-01-01T01:01:01.000001Z";
+        ZonedDateTime timestamp = ZonedDateTime.parse(timestampStr);
+        Date date = Date.from(timestamp.toInstant());
+
+        DateJson dateJson = new DateJson(date);
+
+        JsonObject dateJsonJson = new JsonParser().parse(JsonHandler.getInstance().toJson(dateJson)).getAsJsonObject();
+
+        assertThat(dateJsonJson.get("time").getAsString(), equalTo("2016-01-01T01:01:01Z"));
+    }
+
+    class DateJson {
+        @Expose
+        private Date time;
+
+        public DateJson() {}
+
+        public DateJson(Date time) {
+            this.time = time;
+        }
+
+        public Date getTime() {
+            return time;
+        }
+
+        public void setTime(Date time) {
             this.time = time;
         }
     }
