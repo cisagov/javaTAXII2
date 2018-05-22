17c79998c300c7333dc6b7e9ea1f7ada33638177
allenpreville <allen.preville@xorsecurity.com>
2018-05-22 10:30:07 -0400

    Add ManifestEntry and ManifestResource

---
+++ src/main/java/xor/bcmc/taxii2/resources/ManifestEntry.java
@@ -0,0 +1,104 @@
+package xor.bcmc.taxii2.resources;
+
+import com.google.gson.annotations.Expose;
+import xor.bcmc.taxii2.Identifiable;
+import xor.bcmc.taxii2.validation.Errors;
+
+import java.time.ZonedDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ManifestEntry extends TaxiiResource implements Identifiable<String> {
+    @Expose
+    private String id;
+    @Expose
+    private ZonedDateTime date_added;    // Collection RecToObjectRec
+    @Expose
+    private List<String> versions;
+    @Expose
+    private List<String> mediaTypes;
+
+
+    public ManifestEntry(){
+    }
+
+    public ManifestEntry(String id, ZonedDateTime date_added, List<String> mediaTypes, List<String> versions) {
+        this.id = id;
+        this.date_added = date_added;
+        this.mediaTypes = mediaTypes;
+        this.versions = versions;
+    }
+
+    public ManifestEntry(String id, ZonedDateTime date_added, String mediaType, List<String> versions) {
+        this.id = id;
+        this.date_added = date_added;
+        this.setMediaTypes( mediaType);
+        this.versions = versions;
+    }
+
+
+    // Getters
+    public String getId(){
+        return this.id;
+    }
+
+    public ZonedDateTime getDateAdded()
+    {
+        return this.date_added;
+    }
+
+    public List<String> getMediaTypes() {
+        return mediaTypes;
+    }
+
+    public List<String> getVersions() {
+        return versions;
+    }
+
+    // Setters
+    public void setId(String id){
+        this.id = id;
+    }
+
+    @Override
+    public Identifiable<String> withId(String id) {
+        this.id = id;
+        return this;
+    }
+
+    public void setDateAdded(ZonedDateTime date)
+    {
+        this.date_added = date;
+    }
+
+    public void setMediaTypes(List<String> mediaType)
+    {
+        this.mediaTypes = mediaType;
+    }
+
+    public void setMediaTypes(String mediaType)
+    {
+        if(this.mediaTypes == null)
+        {
+            this.mediaTypes = new ArrayList<String>();
+        }
+        this.mediaTypes.add( mediaType );
+    }
+
+    public void setVersions(List<String> versions) {
+        this.versions = versions;
+    }
+
+    @Override
+    public Errors validate() {
+        Errors errors = new Errors();
+        errors.rejectIfNullOrEmpty("id", this.id);
+        return errors;
+    }
+
+    @Override
+    public boolean isValid() {
+        return validate().isEmpty();
+    }
+}
+
