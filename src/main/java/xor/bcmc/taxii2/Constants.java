4e49d40e9de152d1a3d0fc154acfdf1ba6575100
Adam Pearce <adam.pearce@xorsecurity.com>
2018-11-08 10:14:46 -0500

    Remove spaces in versioned MediaTypes
    Spring Controllers interpret MediaType headers without spaces, and
    the 2.1 WD03 spec also defines them without spaces.

--- src/main/java/xor/bcmc/taxii2/Constants.java
+++ src/main/java/xor/bcmc/taxii2/Constants.java
@@ -14,7 +14,7 @@ public interface Constants {
         @Deprecated
         String OASIS_TAXII = "application/vnd.oasis.taxii+json";
         String TAXII = "application/taxii+json";
-        String TAXII_20 = "application/vnd.oasis.taxii+json; version=2.0";
+        String TAXII_20 = "application/vnd.oasis.taxii+json;version=2.0";
         String TAXII_21 = "application/taxii+json;version=2.1";
         /**
          * This media type was defined in TAXII 2.0 spec,
@@ -23,7 +23,7 @@ public interface Constants {
         @Deprecated
         String TAXII20_STIX = "application/vnd.oasis.stix+json";
         String STIX = "application/stix+json";
-        String STIX_20 = "application/vnd.oasis.stix+json; version=2.0";
+        String STIX_20 = "application/vnd.oasis.stix+json;version=2.0";
         String STIX_21 = "application/stix+json;version=2.1"; //This is how the TAXII 2.1 spec has the header in examples. Need to reference with STIX 2.1 spec
 
         List<String> ALL_TAXII = Arrays.asList(TAXII_21, TAXII, TAXII_20);
