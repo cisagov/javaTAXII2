547258f8526ae117579eb60ae9c4f61bffab210f
robotad <dadamos@bcmcgroup.com>
2020-04-14 14:31:45 -0400

    Add test for negative api-root max_content_length also

--- src/test/java/xor/bcmc/taxii21/resources/ApiRoot21Test.java
+++ src/test/java/xor/bcmc/taxii21/resources/ApiRoot21Test.java
@@ -6,13 +6,25 @@ import xor.bcmc.taxii2.validation.Errors;
 public class ApiRoot21Test
 {
     @Test
-    public void testMaxContentLengthValidation()
-    {
+    public void testMaxContentLengthValidation() {
         ApiRoot21 apiRoot21 = new ApiRoot21()
         .withId("api-root-1")
         .withTitle("Api root 1 title")
         .withDescription("Api root 1 description")
         .withMaxContentLength(0);
+
+        Errors errors = apiRoot21.validate();
+
+        assertEquals(errors.size(), 2);
+    }
+
+    @Test
+    public void testMaxContentLengthValidationNegative() {
+        ApiRoot21 apiRoot21 = new ApiRoot21()
+                .withId("api-root-1")
+                .withTitle("Api root 1 title")
+                .withDescription("Api root 1 description")
+                .withMaxContentLength(-1);
 
         Errors errors = apiRoot21.validate();
 
